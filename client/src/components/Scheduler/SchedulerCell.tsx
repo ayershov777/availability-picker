381e887ae0e42ace7725672fe040614114a7138f
Lu Ouyang <v-luouya@microsoft.com>
2021-06-30 12:13:45 -0700

    :sparkles:scheduler cells to have blue dot on hover @mobile size

--- client/src/components/Scheduler/SchedulerCell.tsx
+++ client/src/components/Scheduler/SchedulerCell.tsx
@@ -1,12 +1,26 @@
 import { useDispatch } from "react-redux";
 import styled from "styled-components";
 import { setSelectedDateAction } from "../../redux/actions";
+import useScreenSize, { Breakpoints, ScreenSize } from '../../hooks/useScreenSize';
 
 const Cell = styled.div`
     border: 1px solid black;
     margin: 2px;
     padding: 4px;
     height: calc(100vw/10);
+ 
+    @media (max-width: ${Breakpoints.Small}px) {
+        border: none;
+        width: calc(100vw/10);
+        line-height: calc(100vw/10);
+        border-radius: 50%;
+        text-align: center;
+        
+        &:hover {
+            background-color: #1a73e8;
+            color: white;
+        }
+    }
 `;
 
 type SchedulerCellProps = {
@@ -23,10 +37,9 @@ function SchedulerCell({ date }: SchedulerCellProps) {
     function handleClick() {
         selectDay();
     }
-
     return (
         <Cell onClick={handleClick}>
-            {date.toDateString().substring(4, 10)}
+            {useScreenSize() > ScreenSize.Small ? date.toDateString().substring(4, 10) : date.getDate()}
         </Cell>
     );
 }

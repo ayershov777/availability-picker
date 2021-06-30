e8085e5bb90d648b2358b66d1d2d10eeb41dce6d
Anthony Yershov <v-anyers@microsoft.com>
2021-06-30 05:50:15 -0700

    :art::label: removed memoization of pure functions. added enum instead of any type for size

--- client/src/hooks/useScreenSize.ts
+++ client/src/hooks/useScreenSize.ts
@@ -1,57 +1,50 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 
 export enum ScreenSize { XSmall, Small, Medium, Large, XLarge, XXLarge };
 
-function getMediaQueryLists() {
-	const queries = [
-		"(max-width: 575px)",
-		"(min-width: 576px) and (max-width: 767px)",
-		"(min-width: 768px) and (max-width: 991px)",
-		"(min-width: 992px) and (max-width: 1199px)",
-		"(min-width: 1200px) and (max-width: 1399px)",
-		"(min-width: 1400px)",
-	];
-	
-	return queries.map((q) => window.matchMedia(q));
+const queries = [
+	"(max-width: 575px)",
+	"(min-width: 576px) and (max-width: 767px)",
+	"(min-width: 768px) and (max-width: 991px)",
+	"(min-width: 992px) and (max-width: 1199px)",
+	"(min-width: 1200px) and (max-width: 1399px)",
+	"(min-width: 1400px)",
+];
+
+const mediaQueryLists = queries.map((q) => window.matchMedia(q));
+
+function getSize () {
+	const isMatch = (idx: ScreenSize) => mediaQueryLists[idx].matches;
+
+	switch(true) {
+		case isMatch(ScreenSize.XSmall):
+			return ScreenSize.XSmall;
+		case isMatch(ScreenSize.Small):
+			return ScreenSize.Small;
+		case isMatch(ScreenSize.Medium):
+			return ScreenSize.Medium;
+		case isMatch(ScreenSize.Large):
+			return ScreenSize.Large;
+		case isMatch(ScreenSize.XLarge):
+			return ScreenSize.XLarge;
+		case isMatch(ScreenSize.XXLarge):
+			return ScreenSize.XXLarge;
+		default:
+			throw new Error("no queries match");
+	}
 }
 
 export default function useScreenSize() {
-	const mediaQueryLists = useMemo(getMediaQueryLists, []);
-
-    const getSize = useCallback(() => {
-		const isMatch = (idx: ScreenSize) => mediaQueryLists[idx].matches;
-
-		switch(true) {
-			case isMatch(ScreenSize.XSmall):
-				return ScreenSize.XSmall;
-			case isMatch(ScreenSize.Small):
-				return ScreenSize.Small;
-			case isMatch(ScreenSize.Medium):
-				return ScreenSize.Medium;
-			case isMatch(ScreenSize.Large):
-				return ScreenSize.Large;
-			case isMatch(ScreenSize.XLarge):
-				return ScreenSize.XLarge;
-			case isMatch(ScreenSize.XXLarge):
-				return ScreenSize.XXLarge;
-			default:
-				return ScreenSize.XSmall;
-		}
-	}, [mediaQueryLists]);
-
 	const [size, setSize] = useState(getSize);
 
     useEffect(() => {
 		const handler = () => setSize(getSize);
-		mediaQueryLists.forEach((mql) =>
-			mql.addEventListener("change", handler)
-		);
+		mediaQueryLists.forEach((mql) => mql.addEventListener("change", handler));
 
-		return () =>
-			mediaQueryLists.forEach((mql) =>
-				mql.removeEventListener("change", handler)
-			);
-	}, [getSize, mediaQueryLists]);
+		return () => (
+			mediaQueryLists.forEach((mql) => mql.removeEventListener("change", handler))
+		);
+	}, []);
 
 	return size;
 }
